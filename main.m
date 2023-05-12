50e46ec1138b7b828f3a2b7af37241865d3c064c
Justin Miller <justinmiller100@gmail.com>
2023-05-11 19:28:55 -0700

    ahhh

--- main.m
+++ main.m
@@ -3,24 +3,28 @@ int main ()
 {
     [NSAutoreleasePool new];
     [NSApplication sharedApplication];
-    [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-    id menubar = [[NSMenu new] autorelease];
-    id appMenuItem = [[NSMenuItem new] autorelease];
+    
+    id menubar = [NSMenu new];
+    id appMenuItem = [NSMenuItem new];
     [menubar addItem:appMenuItem];
     [NSApp setMainMenu:menubar];
-    id appMenu = [[NSMenu new] autorelease];
+
+    id appMenu = [NSMenu new];
     id appName = @"Testing Objective-C";
     id quitTitle = [@"Quit " stringByAppendingString:appName];
-    id quitMenuItem = [[[NSMenuItem alloc] initWithTitle:quitTitle
-        action:@selector(terminate:) keyEquivalent:@"q"] autorelease];
+    id quitMenuItem = [[NSMenuItem alloc] initWithTitle:quitTitle
+        action:@selector(terminate:) keyEquivalent:@"q"];
+        
     [appMenu addItem:quitMenuItem];
     [appMenuItem setSubmenu:appMenu];
-    id window = [[[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 640, 480)
-        styleMask:NSTitledWindowMask backing:NSBackingStoreBuffered defer:NO]
-            autorelease];
+
+    id window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 640, 480)
+        styleMask:NSTitledWindowMask backing:NSBackingStoreBuffered defer:NO];
+    
     [window cascadeTopLeftFromPoint:NSMakePoint(0,0)];
     [window setTitle:appName];
     [window makeKeyAndOrderFront:nil];
+
     [NSApp activateIgnoringOtherApps:YES];
     [NSApp run];
     return 0;
